ce0bf586c02dad2e33c69807dc0fee71b600fcce
BUZZ <113298285+BDTA-zky@users.noreply.github.com>
2023-10-09 22:45:07 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "ChatGPT-BUZZ(备用站①)",
+  description: "BUZZ的1号备用站",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "ChatGPT-BUZZ(备用站①)",
     statusBarStyle: "default",
   },
 };
